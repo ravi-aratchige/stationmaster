2183669c69a5392a3f81f73ecd235410555abec0
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2025-01-16 00:56:55 +0530

    UPDATE: change main.py structure for quick testing

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from chat.agents import ToolBoundAgentBuilder
 
 
 def main():
+    test_question = "Are there trains available from Ja-ela to Colombo Fort?"
     message = input("Your question: ")
     agent = ToolBoundAgentBuilder()
     response = agent.invoke({"input": message})
-    print(response)
+    print(response["output"])
 
 
 if __name__ == "__main__":
