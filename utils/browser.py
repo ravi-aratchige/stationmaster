cc86e13c56544437b08cc83c2c63effdc64cd05f
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2025-01-21 01:26:20 +0530

    UPDATE: disable GPU acceleration

--- utils/browser.py
+++ utils/browser.py
@@ -45,6 +45,7 @@ class BrowserOptionsFactory:
             options.add_argument("--headless=new")
 
         options.add_argument("--no-sandbox")
+        options.add_argument("--disable-gpu")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--window-size=1920,1080")
 
