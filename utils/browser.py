b1e1d9b4167e2a989305e4b579d10d3e0167c844
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2025-01-20 13:23:58 +0530

    REFACTOR: browser config in factory class

---
+++ utils/browser.py
@@ -0,0 +1,74 @@
+"""Set up browser options and other configurations for the webdriver.
+"""
+
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from settings import HEADLESS_MODE
+
+
+class BrowserOptionsFactory:
+    @staticmethod
+    def get_browser_options(browser_name: str = "chrome"):
+        if browser_name.lower() == "chrome":
+            return BrowserOptionsFactory.get_chrome_options()
+        elif browser_name.lower() == "firefox":
+            return BrowserOptionsFactory.get_firefox_options()
+        else:
+            raise ValueError("Invalid browser name has been specified!")
+
+    @staticmethod
+    def get_chrome_options() -> ChromeOptions:
+        """Return Chrome options for Selenium.
+
+        Chrome options for headless browser can be enabled if required.
+
+        Returns:
+            selenium.webdriver.chrome.options: Browser options for Chrome.
+        """
+
+        options = ChromeOptions()
+
+        # Check whether Chrome needs to be run in headless mode
+        if HEADLESS_MODE:
+            options.add_argument("--headless")
+
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+
+        chrome_prefs = {}
+        options.experimental_options["prefs"] = chrome_prefs
+        chrome_prefs["profile.default_content_settings"] = {"images": 2}
+
+        # Prevent images loading
+        chrome_prefs["profile.managed_default_content_settings"] = {"images": 2}
+
+        return options
+
+    @staticmethod
+    def get_firefox_options() -> FirefoxOptions:
+        """Return Firefox options for Selenium.
+
+        Firefox options for headless browser can be enabled if required.
+
+        Returns:
+            selenium.webdriver.firefox.options: Browser options for Firefox.
+        """
+
+        firefox_options = FirefoxOptions()
+
+        # Check whether Firefox needs to be run in headless mode
+        if HEADLESS_MODE:
+            firefox_options.add_argument("--headless")
+
+        firefox_options.add_argument("--no-sandbox")
+        firefox_options.add_argument("--disable-dev-shm-usage")
+
+        # Disable images loading
+        firefox_options.set_preference("permissions.default.image", 2)
+
+        return firefox_options
+
+
+# Make module safely exportable
+if __name__ == "__main__":
+    pass
