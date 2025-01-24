0fd75b13fb180f8e6472453cf090f9495caa4a02
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2025-01-24 13:43:12 +0530

    CREATE: payload for full conversation

--- chat/payloads.py
+++ chat/payloads.py
@@ -1,9 +1,27 @@
+from enum import Enum
+from typing import List
 from pydantic import BaseModel
 
 
-# User input model
-class MessagePayload(BaseModel):
+class MessageAuthor(str, Enum):
+    STATIONMASTER = "stationmaster"
+    HUMAN = "human"
+
+
+# User input model (content only)
+class ContentOnlyMessagePayload(BaseModel):
     content: str
+
+
+# User input model (content and sender)
+class ContentAndSenderMessagePayload(BaseModel):
+    content: str
+    sender: MessageAuthor = MessageAuthor.HUMAN
+
+
+# Complete chat model
+class ChatPayload(BaseModel):
+    messages: List[ContentAndSenderMessagePayload]
 
 
 # Make module safely exportable
