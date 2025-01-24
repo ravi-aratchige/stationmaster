5d284ccfd0768dbc95e920eb9f2a612e875219c3
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2025-01-24 13:43:27 +0530

    ADD: route to test conversation history with agent

--- chat/routes.py
+++ chat/routes.py
@@ -1,6 +1,8 @@
 import time
 from fastapi import APIRouter
-from chat.payloads import MessagePayload
+from langchain_core.messages import HumanMessage
+from langchain_core.messages import AIMessage
+from chat.payloads import ChatPayload, ContentOnlyMessagePayload, MessageAuthor
 from chat.agents import ToolBoundAgentBuilder
 from oracle.retrievers import retrieve_ticket_prices
 
@@ -52,7 +54,7 @@ def test_response_ticket_info():
 
 # Get response from chatbot for user input
 @router.post("/get-response/", tags=["Live"])
-def get_response_from_chatbot(input: MessagePayload):
+def get_response_from_chatbot(input: ContentOnlyMessagePayload):
     message = input.content
     agent = ToolBoundAgentBuilder()
     response = agent.invoke({"input": message})
@@ -69,6 +71,41 @@ def test_retriever():
     output = retrieve_ticket_prices(departure_station, arrival_station)
     return {
         "data": output,
+    }
+
+
+# Get chat history
+@router.post("/test-chat-history/")
+def get_chat_history(chat: ChatPayload):
+    # Instatiate empty list to store chat history
+    # in a format the agent can understand
+    chat_history = []
+
+    # Iterate through messages in chat (except last message)
+    for message in chat.messages[:-1]:
+        if message.sender == MessageAuthor.HUMAN:
+            formatted_msg = HumanMessage(content=message.content)
+        elif message.sender == MessageAuthor.STATIONMASTER:
+            formatted_msg = AIMessage(content=message.content)
+        # Add formatted message to chat history
+        chat_history.append(formatted_msg)
+
+    print(chat_history)
+
+    # Setup stationmaster agent
+    agent = ToolBoundAgentBuilder()
+
+    # Invoke agent with last message in the chat (the user's latest input)
+    # and the formatted chat history
+    response = agent.invoke(
+        {
+            "input": chat.messages[-1].content,
+            "chat_history": chat_history,
+        }
+    )
+
+    return {
+        "data": response,
     }
 
 
